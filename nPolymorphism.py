5a550cff70b791bc99209116d9667e58dacfe65b
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-07-09 15:16:46 +0600

    Create nPolymorphism.py

---
+++ nPolymorphism.py
@@ -0,0 +1,14 @@
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+        
+for animal in [Dog(), Cat()]:
+    animal.sound()
